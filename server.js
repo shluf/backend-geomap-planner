3b6652b50decbaed4c059533902b3f448ff3063b
shluf <150960891+shluf@users.noreply.github.com>
2023-12-24 14:25:13 +0700

    Update server.js port

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const morgan = require('morgan')
 require('dotenv').config()
 const connectDB = require('./api/config/connectDB')
+const port = process.env.PORT || 3000;
 
 //IMPORT ROUTE
 const missionRoutes = require('./api/routes/missionRoute')
@@ -18,8 +19,8 @@ app.use(morgan('dev'))
 //MISSION ROUTES
 app.use('/api/mission', missionRoutes)
 
-// mongoose.connection.once('open', () => {
-//     app.listen(process.env.PORT, () => {
-//         console.log(`listening on port http://localhost:${process.env.PORT} Gamaforce`)
-//     })
-// })
+mongoose.connection.once('open', () => {
+    app.listen(port, () => {
+        console.log(`listening on port http://localhost:${port} Gamaforce`)
+    })
+})
